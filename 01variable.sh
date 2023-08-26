972ed481d40d59e507f753b7dfdf9829e013a05f
viswanath476 <viswanaths476@gmail.com>
2023-08-26 09:13:21 -0700

    01variable.sh added extra date excuting

--- 01variable.sh
+++ 01variable.sh
@@ -3,4 +3,4 @@
 #shell script runthe command inside $() and stores the output into variable called DATE..
 DATE=$(date)
 
-echo "hello, this script is excuted at timestamp: $DATE"
+echo "hello, this script is excuted at timestamp: $DATE" + $(date)
