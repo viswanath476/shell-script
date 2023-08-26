9fff4dddbf881e64324e172bdc9873ef6e8fecb8
viswanath476 <viswanaths476@gmail.com>
2023-08-26 07:43:35 -0700

    hello-world script file added to repo

--- hello-world.sh
+++ hello-world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "hello hi i am learning devops aws"
+echo "Ramesh: hi suresh good moring"
+echo "surresh: hi ramesh very good monrning"
+echo "ramesh how are you suresh"
+echo "suresh: i am fine what about you ramesh"
