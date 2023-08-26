0cb28e58d1d4af6d0010bb7ca5bdb3c827271c17
viswanath476 <viswanaths476@gmail.com>
2023-08-26 16:40:04 -0700

    passing the values instead names

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "hello hi i am learning devops aws"
+person1=$1 #this is the first value from command line
+person2=$2 #this is the 2nd value of the command line
+
+echo "$person1: hi $person2 good moring"
+echo "$person2: hi $person1 very good monrning"
+echo "$person1 how are you $person2"
+echo "$person2: i am fine what about you $person1"
