ba886f8afbc5c3774564b332f27631bce5d1170f
viswanath476 <viswanaths476@gmail.com>
2023-08-26 09:19:22 -0700

    01variable.sh added extra date excuting syam

--- 01variable.sh
+++ 01variable.sh
@@ -2,5 +2,8 @@
 
 #shell script runthe command inside $() and stores the output into variable called DATE..
 DATE=$(date)
+sleep 20
+
+
 
 echo "hello, this script is excuted at timestamp: $DATE" + $(date)
