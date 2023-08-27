e454b65a269b6078b7c5dc49b95887466c4318e0
viswanath476 <viswanaths476@gmail.com>
2023-08-26 18:59:31 -0700

    array

---
+++ 07-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#this is called array,instead of single value it can hold multiple values
+PERSONS=("navya" "bhavya" "sravya" "saanvi")
+#inside array we always refer values with index and it starts from 0
+
+echo "first person: ${PERSONS[0]}"
