bb9e7445ad793481b581ea1c95e38998243daa73
viswanath476 <viswanaths476@gmail.com>
2023-08-27 01:08:18 -0700

    add functions

---
+++ 10-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#out program goal is install mysql
+
+USERID=$(id -u)
+# this functtion should validate the previous command and inform user it is success or failure
+validate(){
+#$1-->it will receive the arugement
+if [ $USERID -ne 0 ]
+then
+    echo "installtion ---Failure"
+    exit 1
+else
+    echo "instaltion ..success"    
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "error::pllease run the script with root access"
+    exit 1
+fi
+#it is our resposisibily again to check installtion is success or not
+yum install mysql -y
+
+validate $?
+
+yum install  postfix -y
+
+validate $?
