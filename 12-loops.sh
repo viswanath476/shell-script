639dff703f179ca0587e6b412c8d2a0a356fdcc4
viswanath476 <viswanaths476@gmail.com>
2023-08-31 20:21:34 -0700

    loops

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+USERID=$(id-u)
+R="\e[31"
+N="\e[0m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit1
+    
 # all arg are in $@
 for i in $@
 do 
