1e7654785fa44d42612384267652925b893a0fdd
viswanath476 <viswanaths476@gmail.com>
2023-08-31 22:33:47 -0700

    loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-LOGSDIR=/home/centos/shellscript
+LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
